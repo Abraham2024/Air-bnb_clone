493f56e882c827adfb69a3c06e588045c16b69c8
Muhammed Ibrahim Amanokhai <ibrahimjunior08@gmail.com>
2023-09-02 15:56:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 var prevScrollpos = window.pageYOffset;
-  window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.querySelector("header").style.top = "0";
-      document.querySelector(".footer").style.bottom = "0";
-    } else {
-      document.querySelector("header").style.top = "0";
-      document.querySelector(".footer").style.bottom = "-98px";
-    }
-    prevScrollpos = currentScrollPos;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("header").style.top = "0";
+    document.querySelector(".footer").style.bottom = "0";
+  } else {
+    document.querySelector("header").style.top = "0";
+    document.querySelector(".footer").style.bottom = "-98px";
   }
+  prevScrollpos = currentScrollPos;
+}
+
+function hideMapButton() {
+  var mapButton = document.querySelector('.map');
+  mapButton.style.display = 'none';
+}
+
+// Set a timer to call the hideMapButton function after 8 seconds
+setTimeout(hideMapButton, 8000);
